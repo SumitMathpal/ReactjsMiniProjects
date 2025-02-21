efaad5d7222e6db356b66d8e784c07a402c51ae7
SumitMathpal <sumitmathpalcse@gmail.com>
2025-02-21 15:32:25 +0530

    Create a Filtering a list App using stateLifting.

---
+++ Filtering a list/src/FilterItems.jsx
@@ -0,0 +1,10 @@
+function FilterItems(items, query) {
+  query = query.toLowerCase();
+  return items.filter(item =>
+    item.name.split(' ').some(word =>
+      word.toLowerCase().startsWith(query)
+    )
+  );
+}
+
+export default FilterItems;
